575868d2e92d0851a58d6138e88f30198205fc2e
CivitasHaw <romaniitedomum@outlook.de>
2019-02-21 23:59:46 +0100

    adding LoginFragment

---
+++ app/src/main/java/com/example/tim/romaniitedomum/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.example.tim.romaniitedomum;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    private static final String TAG = "LoginFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        Log.d(TAG, "onCreateView: called");
+        return view;
+    }
+}
