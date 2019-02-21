df30aafd879e36f90e7700e1f0901b908bdef5b7
CivitasHaw <romaniitedomum@outlook.de>
2019-02-22 00:00:49 +0100

    let app start with LoginFragment

--- app/src/main/java/com/example/tim/romaniitedomum/MainActivity.java
+++ app/src/main/java/com/example/tim/romaniitedomum/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.tim.romaniitedomum;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new LoginFragment());
+        transaction.commit();
     }
 }
