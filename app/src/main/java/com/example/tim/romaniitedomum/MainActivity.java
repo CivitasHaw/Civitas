bec4dd6bae753680299fb93e1b9aa5c9ed508069
CivitasHaw <romaniitedomum@outlook.de>
2019-02-23 23:49:47 +0100

    Google Services Version check

--- app/src/main/java/com/example/tim/romaniitedomum/MainActivity.java
+++ app/src/main/java/com/example/tim/romaniitedomum/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.tim.romaniitedomum;
 
+import android.app.Dialog;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.tim.romaniitedomum.loginRegister.LoginFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, new LoginFragment());
-        transaction.commit();
+        if (isServicesOk()){
+            fragmentSwitch(new LoginFragment(), false, "");
+        }
     }
 
     public void fragmentSwitch(Fragment fragment, boolean toBackstack, String name){
@@ -28,5 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
         }
         transaction.commit();
+    }
+
+    public boolean isServicesOk(){
+        Log.d(TAG, "isServicesOk: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (available == ConnectionResult.SUCCESS){
+            Log.d(TAG, "isServicesOk: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Log.d(TAG, "isServicesOk: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, getResources().getText(R.string.toast_google_service_error), Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
