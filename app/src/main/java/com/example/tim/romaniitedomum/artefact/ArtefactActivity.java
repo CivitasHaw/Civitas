d5c76cde7ade345038b04c6a2aedf88de2b34ee3
CivitasHaw <romaniitedomum@outlook.de>
2019-03-04 00:08:39 +0100

    navigate to NewArtefactActivity and display test data

--- app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactActivity.java
+++ app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactActivity.java
@@ -2,6 +2,8 @@ package com.example.tim.romaniitedomum.artefact;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.tim.romaniitedomum.R;
@@ -18,5 +20,20 @@ public class ArtefactActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artefact);
+
+        fragmentSwitcher2(new NewArtefactFragment(), false, "");
+
+    }
+
+    public void fragmentSwitcher2(Fragment fragment, boolean toBackstack, String name){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_artefact_fragment, fragment);
+        if(toBackstack){
+            transaction.addToBackStack(name);
+        } else {
+
+        }
+        transaction.commit();
+
     }
 }
--- app/src/main/java/com/example/tim/romaniitedomum/artefact/NewArtefactFragment.java
+++ app/src/main/java/com/example/tim/romaniitedomum/artefact/NewArtefactFragment.java
@@ -1,6 +1,9 @@
 package com.example.tim.romaniitedomum.artefact;
 
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +11,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tim.romaniitedomum.ApplicationClass;
+import com.example.tim.romaniitedomum.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by TimStaats 03.03.2019
@@ -15,9 +27,35 @@ import android.view.ViewGroup;
 
 public class NewArtefactFragment extends Fragment {
 
+    private static final String TAG = "NewArtefactFragment";
+
+    private TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_new_artefact, container, false);
+
+        initNewArtefact(view);
+
+        List<Address> adr = new ArrayList<>();
+        Geocoder geocoder = new Geocoder(getContext(), Locale.GERMAN);
+        String name = (String) ApplicationClass.user.getProperty("name");
+        String street = "";
+        Location location = ApplicationClass.mLocation;
+        try {
+            adr = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            street = adr.get(0).getAddressLine(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        textView.setText(name + "\n" + "Address: " + street);
+        return view;
+    }
+
+    private void initNewArtefact(View view) {
+        textView = view.findViewById(R.id.textView);
     }
 }
