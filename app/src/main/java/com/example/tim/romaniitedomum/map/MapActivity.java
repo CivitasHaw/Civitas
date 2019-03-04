1e7e5699d417e10b1386db14c96ca4bbed72ca54
CivitasHaw <romaniitedomum@outlook.de>
2019-03-05 00:28:55 +0100

    close drawer

--- app/src/main/java/com/example/tim/romaniitedomum/map/MapActivity.java
+++ app/src/main/java/com/example/tim/romaniitedomum/map/MapActivity.java
@@ -71,6 +71,7 @@ public class MapActivity extends AppCompatActivity implements NavigationView.OnN
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_map);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -140,6 +141,7 @@ public class MapActivity extends AppCompatActivity implements NavigationView.OnN
                 break;
         }
 
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
