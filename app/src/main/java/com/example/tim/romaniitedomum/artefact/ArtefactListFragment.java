8048feb2fd2f96154deca3544337343f3b68a46f
CivitasHaw <romaniitedomum@outlook.de>
2019-03-07 11:01:06 +0100

    prepare ListOfArtefacts layout and java

--- app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactListFragment.java
+++ app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactListFragment.java
@@ -1,12 +1,18 @@
 package com.example.tim.romaniitedomum.artefact;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tim.romaniitedomum.R;
 
@@ -16,11 +22,84 @@ import com.example.tim.romaniitedomum.R;
 
 public class ArtefactListFragment extends Fragment {
 
+    private static final String TAG = "ArtefactListFragment";
+
+    private View mProgressViewArtefactList;
+    private View mFormViewArtefactList;
+    private TextView tvLoadArtefactList;
+
+    private TextView tvListOfArtefacts;
+
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_artefact_list, container, false);
 
+        initArtefactList(view);
+
         return view;
+    }
+
+
+    private void initArtefactList(View view) {
+
+        mFormViewArtefactList = view.findViewById(R.id.login_form);
+        mProgressViewArtefactList = view.findViewById(R.id.login_progress);
+        tvLoadArtefactList = view.findViewById(R.id.tvLoad);
+
+        tvListOfArtefacts = view.findViewById(R.id.text_list_of_artefacts);
+
+    }
+
+
+
+
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mFormViewArtefactList.setVisibility(show ? View.GONE : View.VISIBLE);
+            mFormViewArtefactList.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mFormViewArtefactList.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressViewArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressViewArtefactList.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressViewArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+            tvLoadArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoadArtefactList.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tvLoadArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressViewArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoadArtefactList.setVisibility(show ? View.VISIBLE : View.GONE);
+            mFormViewArtefactList.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
