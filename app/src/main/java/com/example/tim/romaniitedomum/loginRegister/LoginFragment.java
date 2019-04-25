36a4ac42d7cb1a2d4091c0a74d87cace62ac0113
CivitasHaw <romaniitedomum@outlook.de>
2019-04-25 22:06:51 +0200

    unused libraries removed

--- app/src/main/java/com/example/tim/romaniitedomum/loginRegister/LoginFragment.java
+++ app/src/main/java/com/example/tim/romaniitedomum/loginRegister/LoginFragment.java
@@ -8,7 +8,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,7 +15,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +26,6 @@ import com.backendless.persistence.local.UserIdStorageFactory;
 import com.example.tim.romaniitedomum.ApplicationClass;
 import com.example.tim.romaniitedomum.artefact.Artefact;
 import com.example.tim.romaniitedomum.MainActivity;
-import com.example.tim.romaniitedomum.artefact.ArtefactActivity;
 import com.example.tim.romaniitedomum.map.MapActivity;
 import com.example.tim.romaniitedomum.R;
 
