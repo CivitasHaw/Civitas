9e0c0af325d9f6640a44839a769c54eef94fb558
CivitasHaw <romaniitedomum@outlook.de>
2019-04-25 23:46:25 +0200

    dunno

--- app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactDetailFragment.java
+++ app/src/main/java/com/example/tim/romaniitedomum/artefact/ArtefactDetailFragment.java
@@ -1,8 +1,12 @@
 package com.example.tim.romaniitedomum.artefact;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -25,7 +29,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.backendless.Backendless;
-import com.backendless.Geo;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 import com.backendless.geo.GeoPoint;
